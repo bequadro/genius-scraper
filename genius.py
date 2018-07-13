05b31e07f6f7ba84f069ede79fb0874527128ae2
bequadro <30233933+bequadro@users.noreply.github.com>
2018-07-13 16:02:02 +0200

    small fixes

--- genius.py
+++ genius.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import re
 import pathlib
 import eyed3
@@ -25,24 +26,31 @@ def scrape_song_lyrics_from_url(title, artist):
 if __name__ == "__main__":
 
 	path = sys.argv[1]
+	try:
+		artist = sys.argv[2]
+	except:
+		artist = 0
 
 	print('\nGENIUS SCRAPER')
 
-	with open('api.key', 'r') as f:
+	with open(os.path.join(os.path.dirname(__file__), 'api.key'), 'r') as f:
 		api_key = f.read()
 		print(api_key + '\n')
 	f.close()
 
 	api = genius.Genius(api_key)
 
-	for mp3_file in pathlib.Path(path).glob('*.mp3'):
+	for mp3_file in pathlib.Path(path).glob('**/*.mp3'):
 		tag = eyed3.load(mp3_file).tag
-		song = api.search_song(tag.title, tag.artist)
+		title = tag.title
+		if not artist:
+			artist = tag.artist.split(',')[0]
+		song = api.search_song(title, artist)
 		if song:
 			lyrics = song.lyrics
 		else:
-			lyrics = scrape_song_lyrics_from_url(tag.title, tag.artist)
+			lyrics = scrape_song_lyrics_from_url(title, artist)
 		if lyrics:
 			tag.lyrics.set(lyrics)
-			tag.save()
+		tag.save(version=eyed3.id3.ID3_V2_4)
 		print('')
